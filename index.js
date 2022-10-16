6ac5a933ac24a58254e87a6ed8db2771cb316bc5
moreza-kaze <morezakaze380@gmail.com>
2022-10-17 00:06:24 +0330

    add comment and database

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import http from "http";
 import dotenv from "dotenv";
 import App from "./src/app.js";
+import dataBase from "./src/database/index.js";
 
+//import .env
 const envirment = dotenv.config();
-const server = http.createServer(App);
-const port = process.env.APP_PORT || 8000;
+// crate server
+const server = http.createServer(App.app);
 
+// conect mongoDB to project
+const mongoDB = new dataBase(process.env.DB_HOST);
+mongoDB.mongooseConnect("/learn1");
+//--
+
+//any port free on your sever
+const port = 8000;
 server.listen(port, () => {
-    console.log(`server run on port ${port}`);
+  console.log(`server run on port ${port}`);
 });
