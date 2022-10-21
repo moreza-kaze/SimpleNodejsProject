d31b1293372c2fedd71ab97a6a4b8561c8877f60
moreza-kaze <morezakaze380@gmail.com>
2022-10-21 20:50:36 +0330

    fix bug

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import http from "http";
-import dotenv from "dotenv";
 import App from "./src/app.js";
 import dataBase from "./src/database/index.js";
+import dotenv from "dotenv";
+import Routes from "./src/routes/index.js";
 
-//import .env
-const envirment = dotenv.config();
+//setup .env
+dotenv.config();
+
 // crate server
 const server = http.createServer(App.app);
-
 // conect mongoDB to project
 const mongoDB = new dataBase(process.env.DB_HOST);
-mongoDB.mongooseConnect("/learn1");
-//--
 
+mongoDB.mongooseConnect("/learn1");
+// use router in api
+App.use("/api", Routes);
 //any port free on your sever
-const port = 8000;
+const port = 3000;
 server.listen(port, () => {
   console.log(`server run on port ${port}`);
 });
